bc8c465af124e0d7c06b5d047ab502f587d85fa3
vitalitybo <v2.borisevich@gmail.com>
2018-10-04 18:05:33 +0300

    additional-zeros All tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = number;
+  let currentBase = base;
+  
+  for (let i = 2; i <= currentBase; i++) {
+
+    if (currentBase % i === 0) {
+      let p = 0;
+      let c = 0;
+      let k = number;
+
+      while (currentBase % i === 0) {
+        p++;
+        currentBase /= i;
+      }
+      
+      while (Math.floor(k / i > 0)) {
+        c += Math.floor(k / i);
+        k /= i;
+      }
+
+      count = Math.floor(Math.min(count, c / p));
+    }
+  }
+  
+  return count;
 }
